7b72418255b64b6cc23c4de7124d7fef3935f610
bcthakre <bcthakregcp@gmail.com>
2022-08-28 15:06:00 -0500

    added more props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
 import './App.css';
 import { Greet } from './components/Greet';
+import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
 
 function App() {
+
+  const personName = {
+    first: 'Tim',
+    last: 'Apple'
+  }
+
+  const nameList = [
+    
+    { 
+      first: 'Bruce',
+      last: 'Wayne'
+    },
+    {
+      first: 'Bhupendrasinh',
+      last: 'Thakre'
+    },
+    {
+      first: 'Ziva',
+      last: 'Thakre'
+    }
+  
+]
+
   return (
     <div className="App">
-      <Greet name='Bhupendrasinh' />
+      <Greet name='Bhupendrasinh' messageCount={20} isLoggedIn={false} />
+      <Person name={personName}/>
+      <PersonList names={nameList} />
     </div>
   );
 }
